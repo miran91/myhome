759bb9011ccd209df5a14d090bf9d010e329d2e7
miran <gypsy4go@gmail.com>
2022-12-02 16:26:11 +0900

    course 5-6

---
+++ src/main/java/com/example/myhome/model/Board.java
@@ -0,0 +1,20 @@
+package com.example.myhome.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2-30 자 사이입니다.")
+    private String title;
+    private String content;
+}
